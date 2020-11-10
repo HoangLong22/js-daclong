e6243035b60734a1d15503b42f24facb078bf82a
nguyen long <longnguyenhoang26@gmail.com>
2020-11-10 17:37:14 +0700

    show datajson vào table

---
+++ DataJquery/Content/Js/index.js
@@ -0,0 +1,20 @@
+﻿$('#example').DataTable({
+    data: JSON.parse(dataJson).users,
+    columns: [
+        { data: 'id' },
+        { data: 'name' },
+        { data: 'position' },
+        { data: 'salary' },
+        { data: 'start_date' },
+        { data: 'office' },
+        { data: 'extn' },
+        { data: "", "data": null }
+    ],
+    columnDefs: [
+        {
+            targets: -1,
+            data: null,
+            defaultContent: '<div class="btn-group"><button type="button" class="btn btn-danger"><span><i class="fa fa-trash-alt"></i> Delete</span></button></div>'
+        },
+    ],
+});
