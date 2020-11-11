5f3abccfedb60d22d0b9accc822cbcc4bca9753f
nguyen long <longnguyenhoang26@gmail.com>
2020-11-11 17:39:47 +0700

    hoàn thành clear, detail, search,và giao diện

--- DataJquery/Content/Js/index.js
+++ DataJquery/Content/Js/index.js
@@ -5,42 +5,45 @@
         { data: 'name' },
         { data: function (row) {
                 var positions = JSON.parse(dataJson).positions;
-                var result = '<select class="form-control form-control-sm">';
+                var result = '<select id = "position_' + row["id"] + '" class="form-control form-control-sm">';
                 positions.forEach(function (item) {
                     if (row["position"] == item.id) {
                         result += '<option selected value="' + row["position"] + '">' + item.name + '</option>'
                     }
                     else {
-                        result += '<option value="' + row["position"] + '">' + item.name + '</option>'
+                        result += '<option value="' + item.id + '">' + item.name + '</option>'
 
                     }
                 });
                 result += '</select >';
                 return result;
             }},
-        { data: function (row) {
-                return '<input type="text" id="text" class="form-control form-control-sm" value="' + row["salary"] + '"/>';
+        {
+            data: function (row) {
+                return '<input type="text" id = "salary_' + row["id"] + '" class="form-control form-control-sm" value="' + row["salary"] + '"/>';
         }},
-        { data: function (row) {
-                return '<input type="date" class="form-control form-control-sm" value="' + row["start_date"] + '"/>';
+        {
+            data: function (row) {
+                return '<input type="date" id="date_' + row["id"] + '" class="form-control form-control-sm" value="' + row["start_date"] + '"/>';
         }},
         { data: function (row) {
             var offices = JSON.parse(dataJson).offices;
-            var result = '<select class="form-control form-control-sm">';
+            var result = '<select id="office_'+row["id"] + '" class="form-control form-control-sm">';
             offices.forEach(function (item) {
                 if (row["office"] == item.id) {
                     result += '<option selected value="' + row["office"] + '">' + item.name + '</option>'
                     }
-                    else {
-                    result += '<option value="' + row["office"] + '">' + item.name + '</option>'
+                else {
+                    result += '<option value="' + item.id + '">' + item.name + '</option>'
 
                     }
                 });
                 result += '</select >';
                 return result;
             }},
-        { data: function(row) {
-            return '<input type="text" class="form-control form-control-sm" value="' + row["extn"] + '"/>';
+        {
+            data: function (row) {
+                return '<input type="text" id="extn_' + row["id"] + '" class="form-control form-control-sm" value="' + row["extn"] + '"/>';
         }},
         { data: "", "data": null }
     ],
